904903fd7a93e97f974e8ccb40b437ecd276dbaa
Scott Duncombe <srduncombe@gmail.com>
2012-04-25 09:46:12 -0700

    Cleaning up test asset files

--- public/public/assets-test/application.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript" src="/Users/Scott/.rvm/gems/ruby-1.9.2-p290/gems/jquery-rails-2.0.2/vendor/assets/javascripts/jquery.js"></script>;
-<script type="text/javascript" src="/Users/Scott/.rvm/gems/ruby-1.9.2-p290/gems/jquery-rails-2.0.2/vendor/assets/javascripts/jquery_ujs.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/knockout-latest.debug.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/bindings.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/initialize.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/locationModel.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/maps.js"></script>;
-<script type="text/javascript" src="/Users/Scott/ballot/app/assets/javascripts/application.js"></script>;
