17d770e9852801907dbd128afef5673b1d0e307c
Scott Duncombe <srduncombe@gmail.com>
2012-05-04 14:17:47 -0700

    Better application-wide-JSON handling, also supports JSONP

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,8 +1,27 @@
 class FeedbackController < ApplicationController
   def show
-      feedback = Feedback.find_all_by_choice_key(params['name'])
-      @json = feedback
-      render :partial => "layouts/api.json.erb"
+      feedback = params['name'].nil? ? Feedback.all : Feedback.find_all_by_choice_key(params['name'])
+
+      render :json => feedback.to_json, :callback => params['callback']
+  end
+
+  def update
+      
+      feedback = params['feedback'] || []
+      @json = []
+      
+      feedback.each do |f|
+        feedback = Feedback.find_or_create_by_choice_key_and_user_id(f['choice_key'],f['user_id'])
+        feedback.comment = f['comment']
+        feedback.support = f['support']
+        if feedback.save
+          @json.push({:obj => feedback.id, :success => true })
+        else
+          @json.push({:obj => feedback.id, :success => false })
+        end
+      end
+      
+      render :json => @json.to_json, :callback => params['callback']
   end
 
 
