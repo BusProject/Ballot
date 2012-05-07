62427dff1815aaa251233e097bdac965f07a859b
Scott Duncombe <srduncombe@gmail.com>
2012-05-07 15:03:37 -0700

    Factory for feedback

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,10 +3,17 @@ FactoryGirl.define do
   sequence :email do |n|
     "user#{n}@example.com"
   end
+  sequence :choice_key do |n|
+    "Jefferson Smith ORHD#{n}"
+  end
 
   factory :user do
       email
       password "testactular"
+  end
+  factory :feedback do
+    choice_key
+    user_id { 1 + rand(100) }
   end
 
 end
