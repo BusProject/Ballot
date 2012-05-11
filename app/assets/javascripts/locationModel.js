6061f5d222d4e86ef83fb981c40ac9bdfd459683
Scott Duncombe <srduncombe@gmail.com>
2012-05-11 12:37:02 -0700

    Fixing url in dirtydb ajax call

--- app/assets/javascripts/locationModel.js
+++ app/assets/javascripts/locationModel.js
@@ -187,7 +187,7 @@ function locationModel(data) {
 			})
 		if( reps.length < 1 ) return false
 		$.ajax({
-			url: 'http://localhost:3000/dirtydb/save',
+			url: document.location.href+'/dirtydb/save',
 			type: "POST",
 			data: {data: reps },
 			dataType: "json",
