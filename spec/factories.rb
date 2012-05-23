191fcc375510aa7c0701982bc8ef8a3fcc20a7a3
Scott Duncombe <srduncombe@gmail.com>
2012-05-23 11:34:29 -0700

    Factory for Choice

--- spec/factories.rb
+++ spec/factories.rb
@@ -16,4 +16,18 @@ FactoryGirl.define do
     user_id { 1 + rand(100) }
   end
 
+  sequence :geography do |n|
+    "ORHD#{n}"
+  end
+  sequence :contest do |n|
+    "Mayor of #{n}"
+  end
+
+  factory :choice do
+    geography
+    contest
+    contest_type 'candidate'
+    description 'Now this is the story all about how My life got flipped, turned upside down And I\'d like to take'
+    order { 1 + rand(100) }
+  end
 end
