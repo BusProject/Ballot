32f40ca078e6860491a422d9b70d6edf3f3d6d9c
Scott Duncombe <srduncombe@gmail.com>
2012-05-30 12:33:06 -0700

    Option names are unique inside of choices

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,4 +2,5 @@ class Option < ActiveRecord::Base
   belongs_to :choice
   attr_accessible :blurb, :name, :photo, :position
   has_many :feedback
+  validates_uniqueness_of :name, :scope => :choice_id
 end
