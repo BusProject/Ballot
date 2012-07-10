ba41bc3accf670db09b42f6a14ed7c5b34e8fc45
Scott Duncombe <srduncombe@gmail.com>
2012-07-09 17:42:45 -0700

    Testing gifs

--- app/controllers/draw_controller.rb
+++ app/controllers/draw_controller.rb
@@ -4,24 +4,52 @@ class DrawController < ApplicationController
     require 'RMagick'
 
 
-    file_name = "tmp/done.png"
-    file_path = file_name
-
-    File.delete file_path if File.exists? file_path
-    img = Magick::Image.read("Done.png").first
-    thumb = img.scale(0.25)
+    # For storing the file
+    # If we're storing the file
+    # file_name = "tmp/done.png"
+    # file_path = file_name
+    # File.delete file_path if File.exists? file_path
     
-    msg = Magick::Draw.new
-    msg.font_size(24).font('gothic.ttf').fill('blue')
-    msg.text(60, 35, params[:message])
-    msg.draw thumb
+    if params[:message] == 'png'
+      img = Magick::Image.read("Done.png").first
+      thumb = img.scale(0.25)
     
-    @path = file_name
+      msg = Magick::Draw.new
+      msg.font_size(24).font('gothic.ttf').fill('white')
+      msg.text(60, 35, params[:message])
+      msg.draw thumb
 
-    thumb.write file_path
+      # For storing the file
+      #@path = file_name
+      #thumb.write file_path
+
+
+    else
+
+
+      anim = Magick::ImageList.new
+      anim.read("lib/assets/portraits/busfed-portraits-bygarrettdownen-03958.jpg")
+      # anim1 =  Magick::ImageList.new("lib/assets/portraits/busfed-portraits-bygarrettdownen-03958.jpg")
+      anim.read("lib/assets/portraits/busfed-portraits-bygarrettdownen-03960.jpg")
+      # anim2 = Magick::ImageList.new("lib/assets/portraits/busfed-portraits-bygarrettdownen-03960.jpg")
+      
+      # anim = anim1.composite_layers(anim2)
+      anim.delay = 10
+      anim.format = 'gif'
+      anim.iterations = 0
+      anim.montage
+      
+      anim.optimize_layers(Magick::CoalesceLayer)
+
+
+      
+
+  end
+
 
     respond_to do |format|
       format.png { render :text =>  thumb.to_blob }
+      format.gif { render :text =>  anim.to_blob }
       #format.png { render :text => open(@path, "rb").read }
     end
   end
