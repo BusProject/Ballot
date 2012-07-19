607a8bcbe350f060b626926e2feeca3552e71f2b
Scott Duncombe <srduncombe@gmail.com>
2012-07-18 18:39:10 -0700

    Geolocation prompt

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -25,6 +25,24 @@ function initialize() {
 	} else {
 		
 	}
+
+	// Adding the prompt for Geolocation - sometimes there's a bit of a lag for the browsers API to fire up
+	setTimeout( function() { 
+		if( !yourLocation.geocoded() && navigator.geolocation ) {
+			var position = 'left: 40%; top: 200px;', verb = 'Allow'
+			if( navigator.userAgent.toLowerCase().search('chrome') !== -1  ) position = 'right: 20px;top: 20px;'
+			if( $.browser.mozilla  ) position = 'left: 500px;top: 20px;', verb = 'Share Location'
+
+			$('body').append('<div id="locationNotice" style="opacity: .9; display: none; '+position+'z-index: 1; position: fixed; background: #333; font-size: 50px; color: white; border: 4px solid black; padding: 10px;"><a onclick="$(this).parent().slideUp(\'slow\',function(){ $(this).remove() } ); return false;" style="cursor: pointer; float: right; margin: -10px; font-size: 12px; font-family: sans-serif;">close</a>Click '+verb+'<p style="font-size: 14px; color: #fff;">Your location will not be stored - we\'re<br />just going to use it to build your ballot</p></div>').find('#locationNotice').slideDown('slow')
+
+			$('body').bind('click.location-notice', function() { 
+				$('#locationNotice').slideUp('slow',function(){$(this).remove()})
+				$(this).unbind('click.location-notice')
+			})
+
+			setTimeout(function(){ $('#locationNotice').slideUp('slow',function(){$(this).remove()}) },5000)
+		}
+	}, 750)
 	yourLocation.geocoder( geocoder );
 }
 
