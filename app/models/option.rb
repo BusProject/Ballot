572da928db72862a02240f8a25fdeba50e457b6f
Scott Duncombe <srduncombe@gmail.com>
2012-07-19 17:04:12 -0700

    conditions that work please?

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,7 +2,7 @@ class Option < ActiveRecord::Base
   belongs_to :choice
   attr_accessible :blurb, :name, :photo, :position
 
-  has_many :feedback, :conditions => [ "length(flag)- length(replace( flag,',','') ) <= ? AND approved IS TRUE", 2 ]
+  has_many :feedback, :conditions => [ "length(flag)- length(replace( flag,',','') ) <= ? AND approved IS ?", 2, true ]
   # Don't show anyone with two flags or isn't approved
   validates_uniqueness_of :name, :scope => :choice_id
 end
