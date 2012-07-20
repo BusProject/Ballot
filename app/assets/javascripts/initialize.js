45b891e41942f22e9ad866b4ec82d36dbccf1aa9
Scott Duncombe <srduncombe@gmail.com>
2012-07-20 09:36:36 -0700

    Playing with geolocation prompt position on ipad

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -2,6 +2,34 @@ function initialize() {
 
 	var geocoder = new google.maps.Geocoder()
 
+	var position = 'left: 40%; top: 200px;', verb = 'Allow', fired = false, display = "display: none;"
+	function show() {
+		if( !yourLocation.geocoded() && navigator.geolocation && yourLocation.state == 'front' ) {
+
+			$('body').append('<div id="locationNotice" style="opacity: .9; '+display+position+'z-index: 1; position: fixed; background: #333; font-size: 50px; color: white; border: 4px solid black; padding: 10px;"><a onclick="$(this).parent().slideUp(\'slow\',function(){ $(this).remove() } ); return false;" style="cursor: pointer; float: right; margin: -10px; font-size: 12px; font-family: sans-serif;">close</a>Click '+verb+'<p style="font-size: 14px; color: #fff;">Your location will not be stored - we\'re<br />just going to use it to build your ballot</p></div>').find('#locationNotice').slideDown('slow')
+			fired = true
+
+			$('body').bind('click.location-notice', function() { 
+				$('#locationNotice').slideUp('slow',function(){$(this).remove()})
+				$(this).unbind('click.location-notice')
+			})
+
+			setTimeout(function(){ $('#locationNotice').slideUp('slow',function(){$(this).remove()}) },5000)
+		}
+		
+	}
+
+	if( navigator.userAgent.toLowerCase().search('chrome') !== -1  ) position = 'right: 20px;top: 20px;'
+	if( $.browser.mozilla  ) position = 'left: 500px;top: 20px;', verb = 'Share Location'
+	if( navigator.userAgent.match(/iPad/i) != null ) {
+		'left: 36%; top: 60%', verb = 'Ok', display = "display: block;"
+		show()
+	}
+	
+
+	// Adding the prompt for Geolocation - sometimes there's a bit of a lag for the browsers API to fire up
+	if( ! fired ) setTimeout( function() { show() }, 750)
+
   // Try HTML5 geolocation
   if(navigator.geolocation && yourLocation.state == 'front' ) {
 
@@ -17,35 +45,19 @@ function initialize() {
 					yourLocation.address( results[0].formatted_address )
 				}
 			});
-			$('#locationNotice').slideUp('slow',function(){$(this).remove()})
+			$('#locationNotice').remove()
 		}, 
 		function(error) {
 			console.log(error)
-			$('#locationNotice').slideUp('slow',function(){$(this).remove()})
+			$('#locationNotice').remove()
 		},
 		{maximumAge: 0, enableHighAccuracy: true }
 		);
 	} else {
-		
+
 	}
 
-	// Adding the prompt for Geolocation - sometimes there's a bit of a lag for the browsers API to fire up
-	setTimeout( function() { 
-		if( !yourLocation.geocoded() && navigator.geolocation && yourLocation.state == 'front' ) {
-			var position = 'left: 40%; top: 200px;', verb = 'Allow'
-			if( navigator.userAgent.toLowerCase().search('chrome') !== -1  ) position = 'right: 20px;top: 20px;'
-			if( $.browser.mozilla  ) position = 'left: 500px;top: 20px;', verb = 'Share Location'
 
-			$('body').append('<div id="locationNotice" style="opacity: .9; display: none; '+position+'z-index: 1; position: fixed; background: #333; font-size: 50px; color: white; border: 4px solid black; padding: 10px;"><a onclick="$(this).parent().slideUp(\'slow\',function(){ $(this).remove() } ); return false;" style="cursor: pointer; float: right; margin: -10px; font-size: 12px; font-family: sans-serif;">close</a>Click '+verb+'<p style="font-size: 14px; color: #fff;">Your location will not be stored - we\'re<br />just going to use it to build your ballot</p></div>').find('#locationNotice').slideDown('slow')
-
-			$('body').bind('click.location-notice', function() { 
-				$('#locationNotice').slideUp('slow',function(){$(this).remove()})
-				$(this).unbind('click.location-notice')
-			})
-
-			setTimeout(function(){ $('#locationNotice').slideUp('slow',function(){$(this).remove()}) },5000)
-		}
-	}, 750)
 
 	if( FB ) {
 		FB.getLoginStatus(function(response) {
