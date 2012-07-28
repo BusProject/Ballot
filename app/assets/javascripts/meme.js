39035ac5baccf3c2b500793466e93494dbdf1c6b
Scott Duncombe <srduncombe@gmail.com>
2012-07-27 19:41:12 -0700

    New fonts and backgrounds for memes

---
+++ app/assets/javascripts/meme.js
@@ -0,0 +1,104 @@
+function makeMeme(memeRaw,path,flavor) {
+
+	function memeModel(data) {
+
+		this.quote = ko.observable( data.feedback.comment )
+		this.choices = ko.observableArray([])
+		this.limit = 140
+		this.loading = ko.observable(false)
+
+		for (var i=1; i < 5; i++) {
+			this.choices.push( path+flavor+'/'+i+'.jpg' )
+		};
+
+		this.theme = ko.observable( this.choices()[ Math.floor(Math.random()*4) ] )
+
+		this.quote.fixed = ko.computed( function() { 
+			var limit = this.limit, 
+				copy = this.quote().trim(), 
+				length = copy.length
+			if( length > limit ) {
+				var space = copy.slice(limit-5,length).search(' ')
+				return copy.slice(0,limit+space-1)+'...'
+			} else return copy;
+		}, this)
+		
+		this.special = ko.computed( function() {
+			var quote = this.quote.fixed().trim().toLowerCase(), choices = this.choices()
+			if( quote.search('hey girl') === 0 ) {
+				if( choices.length == 4 ) {
+					this.choices.push(path+'special/gosling.jpg')
+					this.theme( path+'special/gosling.jpg' )
+				}
+			} else if( quote.search('is too damn high') === quote.length - 16 ) {
+				if( choices.length == 4 ) {
+					this.choices.push(path+'special/high.jpg')
+					this.theme( path+'special/high.jpg' )
+				}
+			} else {
+				if( choices[4] )this.choices.remove( choices[4] )
+			}
+		},this)
+
+		this.previewChange = ko.computed(function() {
+			var p = this.quote.fixed(), t = this.theme()
+			this.loading(true)
+		},this)
+
+		this.preview = ko.computed( function() {
+			var loading = this.loading
+			$.post( 
+				document.location.toString().replace('new','preview'),
+				{ quote: this.quote.fixed(), theme: this.theme() }, 
+				function(response) { 
+					$('.preview').html('<a href="data:image/png;base64,' + response + '" target="_blank"><img src="data:image/png;base64,' + response + '" /></a>')
+					loading(false)
+				}
+			)
+		},this).extend({throttle: 500})
+
+
+
+		return this;
+	}
+
+	window.onload = function() { 
+
+		if( !ko && window.parent.ko ) {  // Function loads KO if it's standalone HTML, grabs parent KO if in frame
+			window.ko = window.parent.ko;
+			ko.applyBindings( memeRaw, document.body )
+		} else if ( !ko ) {
+			var add_ko = document.createElement('script')
+			add_ko.src = '<%= asset_path "knockout-latest.debug.js" %>'
+			add_ko.onload = function() { memeRaw = new memeModel(memeRaw); ko.applyBindings( memeRaw = new memeModel(memeRaw), document.body ) }
+			document.body.appendChild(add_ko)
+		} else {
+			ko.applyBindings( memeRaw = new memeModel(memeRaw), document.body )
+		}
+
+		function applyBindings() {
+			$(document.body).
+				on('click','.pick div',function() { ctx = ko.contextFor(this); ctx.$parent.theme( ctx.$data ) } )
+		
+			$('.share-inner',document.body).html( makeShare() )
+		}
+		if( !jQuery && window.parent.jQuery ) { // Loads jQuery if necessary and jQuery UJS
+			window.jQuery = window.parent.jQuery;
+			window.$ = window.parent.jQuery;
+			applyBindings()
+		} else if( !jQuery ) {
+			var add_jQ = document.createElement('script')
+			add_jQ.onload = function() {
+				var add_jQujs = document.createElement('script')
+				add_jQujs.src = '<%= asset_path "jquery_ujs.js" %>'
+				add_jQujs.onload = function() { applyBindings() }
+				document.body.appendChild(add_jQujs)
+			}
+			add_jQ.src = '<%= asset_path "jquery.js" %>'
+			document.body.appendChild(add_jQ)
+		} else {
+			applyBindings()
+		}
+
+	}
+}
