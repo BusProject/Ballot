d02edfa5401d33ee075ec9ca407c611d42618f16
Scott Duncombe <srduncombe@gmail.com>
2012-07-30 11:44:09 -0700

    Moving secret token into .env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ballot::Application.config.secret_token = '5daa6a54d0d2caf566bfab20fb585c8b308ca3784a63f17d9af468ca88b079cb4fe292b506a5354bc21cdbde43d8936057e87a9876e2be0b9f6d459a801beff6'
+Ballot::Application.config.secret_token = ENV['SECRET']
