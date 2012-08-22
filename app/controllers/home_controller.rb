d2499c502929e9d9e9c4453d1b5bf023cd4726d9
Scott Duncombe <srduncombe@gmail.com>
2012-08-21 22:14:04 -0700

    Adding Splash Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if current_user.nil? && !cookies['new_ballot_visitor']
+
+      cookies['new_ballot_visitor'] = {
+        :value => true,
+        :expires => 2.weeks.from_now
+      }      
+      @config = { :state => 'splash' }.to_json
+      @classes = 'splash'
+
+      render :template => 'home/splash.html.erb'
+    else
+      render :template => 'home/index.html.erb' 
+    end
   end
 end
