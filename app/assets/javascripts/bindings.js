89b4d11660c00382fbb88525d2827dc92a68beb3
Scott Duncombe <srduncombe@gmail.com>
2012-09-03 04:13:09 -0700

    Adjusting meme positioning

--- app/assets/javascripts/bindings.js
+++ app/assets/javascripts/bindings.js
@@ -123,7 +123,7 @@ $(document).on('click touchend','#find-ballot .cancel',function(e) { // binding 
 })
 .on('click touchend','.yourFeedback .meme',function(e) {
 	var $data = ko.dataFor(this), memetainer = $('#meme-tainer')
-	if( window.innerHeight < 980 ) memetainer.css('top', Math.floor(120-window.innerHeight/780*120)+'px')
+	if( window.innerHeight < 750 ) memetainer.css('top', ((window.innerHeight < 642 ? 642 : window.innerHeight)-632)+'px')
 	if( window.innerWidth < 1100 ) memetainer.css('marginLeft', (( window.innerWidth < 890 ? 780 : window.innerWidth ) -766-162 ) / 2+'px')
 	$('iframe',memetainer.show() ).attr('src',inits.root+'m/'+$data.you().id+'/new?frame=true' ).parent().prev('#meme-cova').show() 
 	$(document.body).bind('click.meme touchstart.meme',function(e) {
