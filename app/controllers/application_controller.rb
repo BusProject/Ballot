7b39506202e23b684ae69121aeca759e37d431e1
Scott Duncombe <srduncombe@gmail.com>
2012-09-12 11:58:13 -0700

    error screen is ignored on localhost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
- # unless Rails.application.config.consider_all_requests_local
+ unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from ActionController::UnknownAction, with: :render_404
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
- # end
+ end
   
   private
     def render_404(exception)
