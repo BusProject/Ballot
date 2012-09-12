7d27b66eb6ee07e19658ce9b846063c597770ce1
Scott Duncombe <srduncombe@gmail.com>
2012-09-12 11:49:37 -0700

    Fixing searching from error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
- unless Rails.application.config.consider_all_requests_local
+ # unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from ActionController::UnknownAction, with: :render_404
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
- end
+ # end
   
   private
     def render_404(exception)
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
       @content = 
       "
         <script type='text/javascript'> $(document).ready( function() {
-            $('.finder').betterAutocomplete( '/search', function( event, ui ) { document.location = ui.item.url } )
+            $('.finder').betterAutocomplete( '/search', function( event, ui ) { document.location = '/'+ui.item.url } )
             $('.finder').bind('focus.down',function(e) { $(document).scrollTop( $(this).unbind('focus.down').position().top ) })
         })</script>
         <div class='big'>&#161;ERROR!</div>
