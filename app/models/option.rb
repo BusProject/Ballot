012c88d64449d362f4456fce308d4619e9980b8e
Scott Duncombe <srduncombe@gmail.com>
2012-09-13 10:38:56 -0700

    hopefully fixing FB friend error when fb_friends.nil?

--- app/models/option.rb
+++ app/models/option.rb
@@ -4,23 +4,23 @@ class Option < ActiveRecord::Base
 
   has_many :feedback, :conditions => [ "length(flag)- length(replace( flag,',','') ) < ? AND approved = ?", 2, true ], :order => ['cached_votes_up - cached_votes_down DESC'], :limit => 4, :readonly => true do
     def page(offset = 0, limit = 10, current_user=nil)
-      fb_friends = current_user.nil? ? '' : current_user.fb_friends.split(',')
+      fb_friends = current_user.nil? || current_user.fb_friends.nil? ? '' : current_user.fb_friends.split(',')
       fb_friends = '' if fb_friends.empty? || fb_friends.nil?
       all( :readonly => true, :joins => :user, :offset => offset, :limit => limit, :conditions => ["length(feedback.comment) > 1 AND fb NOT IN(?)",  fb_friends ] )
     end
     def friends current_user
-      fb_friends = current_user.nil? ? '' : current_user.fb_friends.split(',')
+      fb_friends = current_user.nil? || current_user.fb_friends.nil? ? '' : current_user.fb_friends.split(',')
       fb_friends = '' if fb_friends.empty? || fb_friends.nil?
       all( :readonly => true, :joins => :user, :conditions => ["length(comment) > 1 AND fb IN(?)", fb_friends ], :limit => nil, :order => "RANDOM()" ) # Returns ALL of your FB friends who've commented on a measure
     end
     def friends_faces current_user
-      fb_friends = current_user.nil? ? '' : current_user.fb_friends.split(',')
+      fb_friends = current_user.nil? || current_user.fb_friends.nil? ? '' : current_user.fb_friends.split(',')
       fb_friends = '' if fb_friends.empty? || fb_friends.nil?
       id = current_user.nil? ? '' : current_user.fb
       all( :readonly => true, :joins => :user, :conditions => ["fb IN(?) AND fb != ?", fb_friends, id ], :order => "RANDOM()" , :limit => 10 )
     end
     def other_faces current_user
-      fb_friends = current_user.nil? ? '' : current_user.fb_friends.split(',')
+      fb_friends = current_user.nil? || current_user.fb_friends.nil? ? '' : current_user.fb_friends.split(',')
       fb_friends = '' if fb_friends.empty? || fb_friends.nil?
       id = current_user.nil? ? '' : current_user.fb
       all( :readonly => true, :joins => :user, :conditions => ["fb NOT IN(?) AND fb != ?", fb_friends,id ], :order => "RANDOM()", :limit => 10 )
