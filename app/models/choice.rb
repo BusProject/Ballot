36afff77e53350dff9d08e55319837976efdfd33
Scott Duncombe <srduncombe@gmail.com>
2012-09-24 21:21:07 -0700

    Added method to make it easier to clean up data

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -12,6 +12,11 @@ class Choice < ActiveRecord::Base
     return ''
   end
   
+  def fullDelete
+    self.options.each{ |o| o.delete } unless self.options.nil?
+    self.delete
+  end
+  
   def geographyNice( stateOnly=true)
     @states = ["The United States of America","Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
     @stateAbvs = ["Prez","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
