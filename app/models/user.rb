48c6d4c10d03571ed62106c19db3fb1371050fbe
Scott Duncombe <srduncombe@gmail.com>
2012-09-27 02:32:50 -0700

    finallly think we've got this figured out

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,10 +44,10 @@ class User < ActiveRecord::Base
   # Method to see if profile name is free
   def check_profile
     unless self.profile.nil?
-      id = self.profile.to_i(16).to_s(16) == self.profile ? self.profile.to_i(16).to_s(10).to_i(2).to_s(10) : 0
+      id = self.profile.to_i(16).to_s(16) == self.profile ? self.profile.to_i(16).to_s(10).to_i(2).to_s(10).to_i(10) : 0
       safe = true 
-      if id != '' # Future proofing all URL names for our first 10^20th users
-        safe = id.to_i(16).to_s(10).to_i(2).to_s(10).to_i(10) > 10e20
+      if id != 0 # Future proofing all URL names for our first 10^20th users
+        safe = id > 10e20
       end
       notstate = self.profile =~ /AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY/
       safe = notstate.nil? && safe
