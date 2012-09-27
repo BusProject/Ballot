4373401b0c498e4a96aa6e83cc996391ba257a9f
Scott Duncombe <srduncombe@gmail.com>
2012-09-27 14:42:06 -0700

    fix profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ActiveRecord::Base
   # Profile and URL related methods
   after_initialize :set_profile
   validate :check_profile
+  before_save :fix_profile
   
   # Method for generating a link to the profile
   def set_profile
@@ -44,7 +45,7 @@ class User < ActiveRecord::Base
   # Method to see if profile name is free
   def check_profile
     unless self.profile.nil?
-      self.profile = self.profile.gsub('/','')
+      self.profile = self.profile.gsub(/\//,//)
       id = self.profile.to_i(16).to_s(16) == self.profile ? self.profile.to_i(16).to_s(10).to_i(2).to_s(10).to_i(10) : 0
       safe = true 
       if id != 0 # Future proofing all URL names for our first 10^20th users
@@ -54,6 +55,10 @@ class User < ActiveRecord::Base
       safe = notstate.nil? && safe
       errors.add( :profile, 'is not unique' ) unless User.where('(id = ? OR profile = ?)  AND id != ?',id,self.profile,self.id).empty? && safe
     end
+  end
+  
+  def fix_profile
+    self.profile = self.profile.gsub(/\//,//) unless self.profile.nil?
   end
   
   def to_url ( full = false)
