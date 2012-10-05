a986035b4e7a18d0545ff679187bed3bbc716302
Scott Duncombe <srduncombe@gmail.com>
2012-10-04 18:23:45 -0700

    can anchor link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,12 @@
 var yourLocation = new locationModel(inits);
 
 window.onload = function() {
-	document.location.hash = '' // Clearing dumb facebook thing
+	if( document.location.hash.length > 1 ){
+		var pos = $( document.location.hash ).position(),
+			top = ( pos || {top:0} ).top-40 
+		document.location.hash = ''
+		$(document).scrollTop( top )
+	} // Clearing dumb facebook thing
 	$('body').removeClass('no-script') //Fixing body class
 	initialize() // This loads goelocation / facebook friends
 	$('.clean-no-script').remove() // Removing non-necessary elements that knockout will just rebuild
