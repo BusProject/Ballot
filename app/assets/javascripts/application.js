6cbb4a672866d2e95b055d1125cf6b28c0e37627
Scott Duncombe <srduncombe@gmail.com>
2012-10-17 18:27:51 -0700

    adjusting scroll-to when doing links to subitems on profile via document hash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,12 @@ var yourLocation = new locationModel(inits);
 
 window.onload = function() {
 	if( document.location.hash.length > 1 ){
-		var pos = $( document.location.hash ).position(),
-			top = ( pos || {top:0} ).top-40 
+		setTimeout( function() { 
+		var pos = $( document.location.hash + ', a[name="'+document.location.hash.replace('#!','')+'"]' ).offset(),
+			top = ( pos || {top:0} ).top-30
 		document.location.hash = ''
 		$(document).scrollTop( top )
+		}, 20)
 	} // Clearing dumb facebook thing
 	$('body').removeClass('no-script') //Fixing body class
 	initialize() // This loads goelocation / facebook friends
