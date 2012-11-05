13641e9ac2231dce9246aca2f55ebefdd904763d
Scott Duncombe <srduncombe@gmail.com>
2012-11-04 19:46:24 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,34 +11,34 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013014411) do
+ActiveRecord::Schema.define(:version => 20121105004443) do
 
   create_table "choices", :force => true do |t|
-    t.string   "contest"
-    t.integer  "order"
-    t.boolean  "commentable",  :default => false
-    t.string   "geography"
-    t.text     "description"
-    t.string   "contest_type"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-    t.integer  "votes",        :default => 1
+    t.string    "contest"
+    t.integer   "order"
+    t.boolean   "commentable",  :default => false
+    t.string    "geography"
+    t.text      "description"
+    t.string    "contest_type"
+    t.timestamp "created_at",                      :null => false
+    t.timestamp "updated_at",                      :null => false
+    t.integer   "votes",        :default => 1
   end
 
   add_index "choices", ["geography", "contest"], :name => "index_choices_on_geography_and_contest", :unique => true
 
   create_table "feedback", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "option_id"
-    t.boolean  "approved",           :default => true
-    t.text     "comment"
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
-    t.text     "flag",               :default => ""
-    t.integer  "choice_id"
-    t.integer  "cached_votes_total", :default => 0
-    t.integer  "cached_votes_up",    :default => 0
-    t.integer  "cached_votes_down",  :default => 0
+    t.integer   "user_id"
+    t.integer   "option_id"
+    t.boolean   "approved",           :default => true
+    t.text      "comment"
+    t.timestamp "created_at",                           :null => false
+    t.timestamp "updated_at",                           :null => false
+    t.text      "flag",               :default => ""
+    t.integer   "choice_id"
+    t.integer   "cached_votes_total", :default => 0
+    t.integer   "cached_votes_up",    :default => 0
+    t.integer   "cached_votes_down",  :default => 0
   end
 
   add_index "feedback", ["cached_votes_down"], :name => "index_feedback_on_cached_votes_down"
@@ -48,10 +48,10 @@ ActiveRecord::Schema.define(:version => 20121013014411) do
   add_index "feedback", ["user_id"], :name => "index_feedback_on_user_id"
 
   create_table "matches", :force => true do |t|
-    t.string   "latlng"
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string    "latlng"
+    t.text      "data"
+    t.timestamp "created_at", :null => false
+    t.timestamp "updated_at", :null => false
   end
 
   add_index "matches", ["latlng"], :name => "index_matches_on_latlng", :unique => true
@@ -85,50 +85,51 @@ ActiveRecord::Schema.define(:version => 20121013014411) do
     t.string   "blurb_source"
     t.string   "party"
     t.boolean  "incumbant",    :default => false
+    t.string   "vip_id"
   end
 
   add_index "options", ["choice_id"], :name => "index_options_on_choice_id"
   add_index "options", ["name"], :name => "index_options_on_name"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.string   "image"
-    t.string   "name"
-    t.string   "location"
-    t.string   "url"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "admin",                  :default => false
-    t.string   "authentication_token"
-    t.string   "guide_name"
-    t.string   "fb"
-    t.boolean  "banned",                 :default => false
-    t.boolean  "deactivated",            :default => false
-    t.text     "description"
-    t.text     "fb_friends"
-    t.text     "alerts"
-    t.text     "pages"
-    t.string   "profile"
-    t.string   "primary"
-    t.string   "secondary"
-    t.string   "bg"
-    t.string   "header_file_name"
-    t.string   "header_content_type"
-    t.integer  "header_file_size"
-    t.datetime "header_updated_at"
-    t.integer  "match_id"
-    t.string   "address"
+    t.string    "email",                  :default => "",    :null => false
+    t.string    "encrypted_password",     :default => "",    :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",          :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at",                                :null => false
+    t.timestamp "updated_at",                                :null => false
+    t.string    "image"
+    t.string    "name"
+    t.string    "location"
+    t.string    "url"
+    t.string    "first_name"
+    t.string    "last_name"
+    t.boolean   "admin",                  :default => false
+    t.string    "authentication_token"
+    t.string    "guide_name"
+    t.string    "fb"
+    t.boolean   "banned",                 :default => false
+    t.boolean   "deactivated",            :default => false
+    t.text      "description"
+    t.text      "fb_friends"
+    t.text      "alerts"
+    t.text      "pages"
+    t.string    "profile"
+    t.string    "primary"
+    t.string    "secondary"
+    t.string    "bg"
+    t.string    "header_file_name"
+    t.string    "header_content_type"
+    t.integer   "header_file_size"
+    t.timestamp "header_updated_at"
+    t.integer   "match_id"
+    t.string    "address"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
