e24936ff5c43400921951c40f53a328a6b0db2a9
Scott Duncombe <srduncombe@gmail.com>
2012-11-05 19:22:08 -0700

    also sync dc

--- script/grabit.rb
+++ script/grabit.rb
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ruby 
 require 'RestClient'
 
-states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+states = ["DC","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
 states.each do |state|
   
