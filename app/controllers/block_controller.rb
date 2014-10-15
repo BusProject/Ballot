2cbb5074f5702b422aafb650aa492ed8b659c33e
Scott Duncombe <srduncombe@gmail.com>
2014-10-15 12:06:10 -0700

    put block order back into create:

--- app/controllers/block_controller.rb
+++ app/controllers/block_controller.rb
@@ -24,6 +24,7 @@ class BlockController < ApplicationController
         @block.option_id = params[:option_id]
       end
       @block.title = params[:title]
+      @block.order = params[:block_order]
       @block.content = params[:content]
       if @block.save
         flash[:notice] = t('guide.block_creation_success')
