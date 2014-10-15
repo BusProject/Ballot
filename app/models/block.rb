7a0b242188fa7f4f5ddefb4a1daf8ad6612029c2
Scott Duncombe <srduncombe@gmail.com>
2014-10-15 12:00:52 -0700

    make block_order attr accessible

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,5 +2,5 @@ class Block < ActiveRecord::Base
   belongs_to :guide
   belongs_to :option
   belongs_to :user_option
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :block_order
 end
