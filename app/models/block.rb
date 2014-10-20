b82e2ecea66edcc32887655a312ca4ea510ea674
TeslaDethray <tesladethray@gmail.com>
2014-10-20 15:57:04 -0700

    Resolved merge issues

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,9 +2,5 @@ class Block < ActiveRecord::Base
   belongs_to :guide
   belongs_to :option
   belongs_to :user_option
-<<<<<<< Updated upstream
   attr_accessible :content, :title, :block_order, :geography
-=======
-  attr_accessible :content, :title, :block_order
->>>>>>> Stashed changes
 end
